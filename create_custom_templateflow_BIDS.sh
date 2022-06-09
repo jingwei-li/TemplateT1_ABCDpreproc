ae16036ff74bea4b9de42272f452ae765f45337c
jingwei-li <allhappyliferay@gmail.com>
2022-06-09 16:33:02 +0200

    Change template resolution
    Why:
    
    * Felix has created 1mm templates by using the same 1.5mm warp image of
    each subject as previous for normalization of the T1w images with
    0.9375x0.9375x1mm resoulation
    
    This change addresses the need by:
    
    *

--- create_custom_templateflow_BIDS.sh
+++ create_custom_templateflow_BIDS.sh
@@ -8,23 +8,18 @@ age_max="12"
 
 for eth in AA EA; do
     mkdir -p $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}
-    gzip -c $PNCtpl_dir/Template_${age_min}${age_max}${eth}/age${age_min}-${age_max}_${eth}.nii > \
+
+    # 1mm as resolution 1
+    gzip -c $PNCtpl_dir/Template_${age_min}${age_max}${eth}/age${age_min}-${age_max}_${eth}_1mm.nii > \
         $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_T1w.nii.gz
     gzip -c $PNCtpl_dir/Template_${age_min}${age_max}${eth}/age${age_min}-${age_max}_${eth}_brainmask.nii > \
         $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_desc-brain_mask.nii.gz
 
-    flirt -in $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_T1w.nii.gz \
-        -ref $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_T1w.nii.gz \
-        -applyisoxfm 1 -nosearch \
-        -out $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_T1w.nii.gz
-
-    flirt -in $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_desc-brain_mask.nii.gz \
-        -ref $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-01_desc-brain_mask.nii.gz \
-        -applyisoxfm 1 -nosearch \
-        -out $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_desc-brain_mask.nii.gz
-    
-    mri_binarize --i $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_desc-brain_mask.nii.gz \
-        --o $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_desc-brain_mask.nii.gz --min 0.4999
+    # 1.5mm as resolution 2
+    gzip -c $PNCtpl_dir/Template_${age_min}${age_max}${eth}/age${age_min}-${age_max}_${eth}.nii > \
+        $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_T1w.nii.gz
+    gzip -c $PNCtpl_dir/Template_${age_min}${age_max}${eth}/age${age_min}-${age_max}_${eth}_brainmask.nii > \
+        $TPL_DIR/tpl-PNC${eth}${age_min}${age_max}/tpl-PNC${eth}${age_min}${age_max}_res-02_desc-brain_mask.nii.gz
 
     # create json file
     python3 create_tf_json.py $eth $age_min $age_max $TPL_DIR/tpl-PNC${eth}${age_min}${age_max} 
