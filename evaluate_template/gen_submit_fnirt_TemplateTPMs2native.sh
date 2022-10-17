1082845766bbb134fb0ea9bd28b3151e36ca0d71
jingwei-li <allhappyliferay@gmail.com>
2022-10-17 17:40:18 +0200

    Add uncommitted gen_submit file changes
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- evaluate_template/gen_submit_fnirt_TemplateTPMs2native.sh
+++ evaluate_template/gen_submit_fnirt_TemplateTPMs2native.sh
@@ -28,8 +28,8 @@ for grp in AA EA; do
     TPMdir="$proj_dir/data/PNC_LauraW/Templates/Template_912$grp"
     # loop over all subjects
     for s in $(cat $subj_ls); do
-        ref_dir="$proj_dir/data/ABCD_datalad/tissue_seg/seg_individual_native/$s"
-        out_dir="$proj_dir/data/ABCD_datalad/tissue_seg/templateTPMs_to_native/Template_912$grp/$s"
+        ref_dir="$proj_dir/data/ABCD_datalad/tissue_seg/seg_individual_native_reconall_EA/$s"
+        out_dir="$proj_dir/data/ABCD_datalad/tissue_seg/templateTPMs_to_native_reconall_EA/Template_912$grp/$s"
         printf "arguments = -s $s -g $grp -t $TPMdir -r $ref_dir -o $out_dir \n"
         printf "log       = ${LOGS_DIR}/\$(Cluster).\$(Process).${s}.log\n"
         printf "output    = ${LOGS_DIR}/\$(Cluster).\$(Process).${s}.out\n"
--- evaluate_template/gen_submit_tissue_seg_fast.sh
+++ evaluate_template/gen_submit_tissue_seg_fast.sh
@@ -8,8 +8,8 @@ RAM='5G'
 LOGS_DIR=$proj_dir/data/ABCD_datalad/code/logs/tissue_seg_fast
 
 subj_ls="$proj_dir/data/ABCD_datalad/code/TemplateT1_ABCDpreproc/lists/subj_ls_all_rand50EA50AA_subset40x2.txt"
-in_dir="$proj_dir/data/ABCD_datalad/preprocessed/PNCAA912/freesurfer"
-out_dir="$proj_dir/data/ABCD_datalad/tissue_seg/seg_individual_native"
+in_dir="$proj_dir/data/ABCD_datalad/preprocessed/PNCEA912/freesurfer"
+out_dir="$proj_dir/data/ABCD_datalad/tissue_seg/seg_individual_native_reconall_EA"
 
 # create the logs dir if it doesn't exist
 [ ! -d "${LOGS_DIR}" ] && mkdir -p "${LOGS_DIR}"
@@ -27,7 +27,8 @@ executable     = $DIR/tissue_segment_fast.sh
 \n"
 
 # loop over all subjects
-for sub in $(cat $subj_ls); do
+#for sub in $(cat $subj_ls); do
+for sub in sub-NDARINVW0VV53Y4; do
     printf "arguments = -s $sub -i $in_dir -o $out_dir \n"
     printf "log       = ${LOGS_DIR}/\$(Cluster).\$(Process).${sub}.log\n"
     printf "output    = ${LOGS_DIR}/\$(Cluster).\$(Process).${sub}.out\n"
